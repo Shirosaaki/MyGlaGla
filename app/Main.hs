4f13c2da06b670b7158e905c2d57179a9189f5cf
shirosaaki <matheo.campanella@epitech.eu>
2025-11-18 12:03:48 +0100

    [+] better ci/cd

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,9 @@
+{-
+-- EPITECH PROJECT, 2025
+-- Main module
+-- File description:
+-- main of the glory glados
+-}
 module Main (main) where
 
 import Lib
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,9 @@
+{-
+-- EPITECH PROJECT, 2025
+-- Main test
+-- File description:
+-- lib of the glory glados
+-}
 module Lib
     ( someFunc
     ) where
